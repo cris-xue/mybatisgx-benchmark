71af12fe860edd4a86ac9ad9d43a798cc5b40b1f
cris <xcc137396549@foxmail.com>
2026-05-23 14:38:34 +0800

    [IMP] 修改性能测试代码

--- mybatis/src/main/java/com/benchmark/mybatis/mapper/MybatisUserMapper.java
+++ mybatis/src/main/java/com/benchmark/mybatis/mapper/MybatisUserMapper.java
@@ -14,21 +14,17 @@ public interface MybatisUserMapper {
 
     int insert(User user);
 
-    int insertBatch(@Param("list") List<User> userList);
-
     int updateById(User user);
 
     int updateSelectiveById(User user);
-
-    int updateBatchById(@Param("list") List<User> userList);
 
     User findById(@Param("id") Long id);
 
     User findByIdAndAgeAndStatus(@Param("id") Long id, @Param("age") Integer age, @Param("status") Integer status);
 
     User findByIdAndUsernameLikeAndAgeGtAndStatusIn(@Param("id") Long id, @Param("username") String username,
-                                                     @Param("age") Integer age, @Param("statusList") List<Integer> statusList);
+                                                    @Param("age") Integer age, @Param("statusList") List<Integer> statusList);
 
     List<User> findDynamicByIdAndUsernameLikeAndAgeGtAndStatusIn(@Param("id") Long id, @Param("username") String username,
-                                                                   @Param("age") Integer age, @Param("statusList") List<Integer> statusList);
+                                                                 @Param("age") Integer age, @Param("statusList") List<Integer> statusList);
 }
